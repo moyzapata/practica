9fbb764d5b7003328fcfd5954589d3ee46f529e4
moyzapata <moyzapata99@gmail.com>
2024-05-13 21:15:16 -0500

    intento post

--- frond-end/src/ui/home/HomeScreen.js
+++ frond-end/src/ui/home/HomeScreen.js
@@ -1,5 +1,6 @@
-import { Box, Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { Box, ButtonBase, Container, CssBaseline, Grid, Paper, ThemeProvider, Typography, createTheme } from '@mui/material';
 import * as React from 'react';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const defaultTheme = createTheme();
 
@@ -10,13 +11,57 @@ function HomeScreen() {
                 <CssBaseline />
                 <Box
                     sx={{
-                        marginTop: 8,
+                        marginTop: 14,
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                     }}
                 >
-                    <p>Hola</p>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            margin: 'auto',
+                            maxWidth: 900,
+                            flexGrow: 1,
+                            backgroundColor: (theme) =>
+                                theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                        }}
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <ButtonBase sx={{ width: 128, height: 128 }}>
+                                    <LockOutlinedIcon />
+                                </ButtonBase>
+                            </Grid>
+                            <Grid item xs={12} sm container>
+                                <Grid item xs={12} direction="row" spacing={2}>
+                                    <Grid item xs>
+                                        <Typography variant="subtitle1" component="div">
+                                            $19.00
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Typography variant="subtitle1" component="div">
+                                            $19.00
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs container direction="column" spacing={2}>
+                                    <Grid item xs>
+                                        <Typography gutterBottom variant="subtitle1" component="div">
+                                            Standard license
+                                        </Typography>
+                                        <Typography variant="body2" gutterBottom>
+                                            Full resolution 1920x1080 • JPEG
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            ID: 1030114
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
                 </Box>
             </Container>
         </ThemeProvider>
